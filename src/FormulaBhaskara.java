d7755e3f5cba3b7b2a162862bfcde6d3ab61cc74
ItamaraCristina <itamara_1245@hotmail.com>
2022-11-27 18:08:02 -0300

    Ex.04 Formula Bhaskara

--- src/FormulaBhaskara.java
+++ src/FormulaBhaskara.java
@@ -7,7 +7,7 @@ public class FormulaBhaskara {
         double C = 5.1;
         double DELTA = (B*B) - 4*A*C;
         R1 = ((B*-1) + Math.sqrt(DELTA))/(2*A);
-        R2 = ((B*-1) - Math.sqrt(DELTA))/(2*A);
+        R2 = ((B*-1) - Math.sqrt(DELTA))/(2*A); 
         
         if(A != 0 && DELTA >= 0){
             System.out.printf("R1 = %.5f\n", R1);
