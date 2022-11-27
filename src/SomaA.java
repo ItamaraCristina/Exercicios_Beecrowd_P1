9343b2e6e7505467b581302999351e6c0897fe68
ItamaraCristina <itamara_1245@hotmail.com>
2022-11-27 17:52:23 -0300

    Ex.03 SomaA

---
+++ src/SomaA.java
@@ -0,0 +1,8 @@
+public class SomaA {
+    public static void main(String[] args) {
+        int A = 36;
+        int B = 14;
+        int SOMA = A + B;
+        System.out.println("SOMA = " + SOMA);
+    }
+}
