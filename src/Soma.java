b2e8e7b5349eb986059279c90a751b413bcf937e
ItamaraCristina <itamara_1245@hotmail.com>
2022-11-27 17:47:58 -0300

    Ex.02 Soma

---
+++ src/Soma.java
@@ -0,0 +1,8 @@
+public class Soma {
+    public static void main(String[] args) {
+        int A = 28;
+        int B = 10;
+        int X = A + B;
+        System.out.println("X = " + X);
+    }
+}
